00f23f08fef40bf5f9b561ce9410f4e54de1b023
Makoorr <azizbenrejeb@gmail.com>
2022-04-02 16:09:43 +0100

    First Commit!

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+    require_once 'assets/php-mailer/Exception.php';
+    require_once 'assets/php-mailer/PHPMailer.php';
+    require_once 'assets/php-mailer/SMTP.php';
+
+    $mail = new PHPMailer();
+
+    try{
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = ''; //your email
+        $mail->Password = ''; //your password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('sbrpara1@gmail.com');
+    }catch(Exception $e) {
+        // echo('<div class="alert-error">
+        //           <span>'.$e->getMessage().'</span>
+        //         </div>');
+    }
+?>
