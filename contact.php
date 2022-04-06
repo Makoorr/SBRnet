b344f2c1316cd40c546e433b82ef0f510b96f9a3
Makoorr <azizbenrejeb@gmail.com>
2022-04-06 16:14:15 +0100

    Fixed footer

--- contact.php
+++ contact.php
@@ -171,7 +171,7 @@
     <!--Section: Contact-->
 
     <!-- ======= Footer ======= -->
-    <footer id="footer">
+    <footer id="footer" style="height: 100%;">
       <!-- Copyright -->
       <div class="container d-md-flex py-4">
 
