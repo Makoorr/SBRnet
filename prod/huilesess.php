59f68483d54fb9b7b54f228546dbd1ab78984a17
Makoorr <azizbenrejeb@gmail.com>
2022-04-13 02:08:59 +0100

    Added Prod Folder

--- prod/huilesess.php
+++ prod/huilesess.php
@@ -61,11 +61,11 @@ if(empty($e)){
 
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.php" class="logo me-auto"><img src = "../assets/img/logosbr.png" alt="" class="img-fluid"></a> -->
-        <h1 class="logo me-auto"><a href="index.php"><img src = "../assets/img/logosbr.png" alt="" class="img-fluid" style="padding-top: 2px;"><span style="color: 00ffb4;font-size: large;font-weight: 500 !important;">  SBR-Pharma</span> </a></h1>
+        <h1 class="logo me-auto"><a href="../index.php"><img src = "../assets/img/logosbr.png" alt="" class="img-fluid" style="padding-top: 2px;"><span style="color: 00ffb4;font-size: large;font-weight: 500 !important;">  SBR-Pharma</span> </a></h1>
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-                <li><a class="active" href="index.php">Accueil</a></li>
+                <li><a class="active" href="../index.php">Accueil</a></li>
                 <li class="dropdown"><a style="cursor: pointer;"><span>Nos Produits</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                     <?php
@@ -79,7 +79,7 @@ if(empty($e)){
                     }
                     ?>
                     </ul>
-                <li><a href="contact.php">Contact</a></li>
+                <li><a href="../contact.php">Contact</a></li>
                 </li>
 
                 <li class="cart-icon">
@@ -100,7 +100,7 @@ if(empty($e)){
                         <h5>DT <h5 id="totalprice">0</h5></h5>
                     </div>
                     <div class="select-button">
-                        <a href="commander.php" class="primary-btn checkout-btn">Commander</a>
+                        <a href="../commander.php" class="primary-btn checkout-btn">Commander</a>
                     </div>
                 </div>
                 </li>
