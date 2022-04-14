ff7a5cb3f36c2509bfc410d5267bcf305bc105d1
Makoorr <azizbenrejeb@gmail.com>
2022-04-14 21:15:08 +0100

    added header includes to prods

---
+++ prod/includes/footer.php
@@ -0,0 +1,16 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" style="height: 100%;">
+    <!-- Copyright -->
+    <div class="container d-md-flex py-4">
+
+    <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> | <strong><span> SBR-Pharma</span></strong>
+        </div>
+    </div>
+    <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <strong><span>Notre page facebook&nbsp;&nbsp;</span></strong>
+        <a href="https://www.facebook.com/Produits-Parapharmaceutiques-Cosm%C3%A9tiques-124308735628194" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+    </div>
+    </div>
+</footer><!-- End Footer -->
