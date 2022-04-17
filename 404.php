e06d2f8cdc0d89c18742bce2adba70f0f98c5fd5
Makoorr <azizbenrejeb@gmail.com>
2022-04-17 07:28:14 +0100

    Page 404 créeé

---
+++ 404.php
@@ -0,0 +1,41 @@
+<?php
+    include("config.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SBRPharma</title>
+    <link rel = "icon" type = "image/png" href = "assets/img/iconsbr.png">
+
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> <!--logos-->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendor/ajax/magnific-popup.css">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <script src="assets/vendor/jQuery/jquery.js"></script>
+    <script src="assets/vendor/jQuery/isotope-pkgd.js"></script>
+    <script src="assets/vendor/jQuery/magnific-popup.js"></script>
+</head>
+<body>
+    <div class="container">
+        <?php include("includes/header.php");?>
+        
+        <section class="mb-4" style="padding-top: 25%;padding-bottom: 15%;height:46em;">
+            <div class="container" style="text-align: center;">
+                <h1>Page introuvable!</h1>
+                <p>Nous somme désolés, on n'a pas pu trouver cette page.</p>
+            </div>
+        </section>
+    </div>
+        <?php include("includes/footer.php");?>
+        <?php include("includes/scripts.php");?>
+</body>
+</html>
