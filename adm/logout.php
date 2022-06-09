6b36b350ec3bf19704e75451e3807f55ec811404
Makoorr <azizbenrejeb@gmail.com>
2022-06-09 18:31:48 +0100

    removed adm

--- adm/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["authentication"]);
-    unset($_SESSION["auth_user"]);
-
-    $_SESSION['message'] = "Logged Out Successfully";
-    header("Location: loginpage.php");
-    exit(0);
-?>
