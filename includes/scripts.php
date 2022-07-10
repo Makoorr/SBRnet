c0865a4c19240d896be548b0c687a357f7541ee6
Makoorr <azizbenrejeb@gmail.com>
2022-07-10 21:36:28 +0100

    fixed bootstrap js file

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,6 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.0/swiper-bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
 <script src="assets/js/main.js"></script>
--- prod/includes/scripts.php
+++ prod/includes/scripts.php
@@ -3,5 +3,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.0/swiper-bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
-<script src="../../assets/js/main.js"></script>
+<script src="../assets/js/main.js"></script>
 <script src="https://kit.fontawesome.com/0552f5e21b.js" crossorigin="anonymous"></script>
